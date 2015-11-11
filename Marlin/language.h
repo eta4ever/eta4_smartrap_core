6db378501374462993663fbdeaa64655cd8c98f2
eta4ever <arlorlo@ya.ru>
2015-11-11 18:31:51 +0300

    немного допиливаний для репрап дисконт фулграфика

--- Marlin/language.h
+++ Marlin/language.h
@@ -27,7 +27,7 @@
 
 #ifndef LANGUAGE_INCLUDE
   // pick your language from the list above
-  #define LANGUAGE_INCLUDE GENERATE_LANGUAGE_INCLUDE(ru)
+  #define LANGUAGE_INCLUDE GENERATE_LANGUAGE_INCLUDE(en)
 #endif
 
 #define PROTOCOL_VERSION "1.0"
